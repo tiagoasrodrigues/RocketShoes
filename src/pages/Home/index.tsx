63047a8e03877774c9b725b82e8893e7bf90fc75
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-03-24 16:37:21 -0300

    finished with 1 test error

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import { ProductList } from './styles';
@@ -25,9 +25,10 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount
+    return sumAmount
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
@@ -42,25 +43,25 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id)
   }
 
   return (
     <ProductList>
       {products.map(product => (
-        <li>
-          <img src={product.image} alt="Tênis de Caminhada Leve Confortável" />
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
           <strong>{product.title}</strong>
           <span>{product.price}</span>
           <button
             type="button"
             data-testid="add-product-button"
-          // onClick={() => handleAddProduct(product.id)}
+            onClick={() => handleAddProduct(product.id)}
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */} 2
-          </div>
+              {cartItemsAmount[product.id] || 0}
+            </div>
 
             <span>ADICIONAR AO CARRINHO</span>
           </button>
